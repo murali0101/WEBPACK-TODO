c6dd1cec7d787366c186d91e67cc673dcd2dad4c
murali0101 <muralimv0101@gmail.com>
2022-04-06 23:38:59 +0530

    WEBPACK-TODO

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import "./index.css";
+
+var header = document.getElementById("header");
+let heading = document.createElement("h1");
+heading.innerHTML = "TODO LIST";
+heading.classList.add("redcolor", "center", "bgheading");
+header.append(heading);
+let logo = document.createElement("img");
+
+logo.src =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzQyhNbxqWHTeDOvfaBtR9cPyJuLAL23x3TQ&usqp=CAU";
+
+header.append(logo);
+var addTodoData = document.getElementById("addTodoData");
+let addTodo = document.createElement("div");
+addTodo.setAttribute("id", "addTodo");
+
+let input = document.createElement("input");
+input.setAttribute("id", "input_taking");
+let btn = document.createElement("button");
+btn.innerHTML = "Add";
+btn.addEventListener("click", () => {
+  let val = document.getElementById("input_taking").value;
+  let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+  todoList.push(val);
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+  display(todoList);
+});
+addTodoData.append(input, btn);
+
+addTodoData.append(addTodo);
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+display(todoList);
+function display(data) {
+  let displayTodo = document.getElementById("displayTodo");
+  displayTodo.innerHTML = "";
+  data.map((ele) => {
+    let title = document.createElement("p");
+    title.innerHTML = ele;
+    displayTodo.append(title);
+  });
+}
